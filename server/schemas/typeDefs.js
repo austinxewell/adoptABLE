6ce1d2a1e988002df6c0f62c3b44ff3fc8631e75
AustinxEwell <austin.ewell86@gmail.com>
2021-12-23 14:47:33 -0700

    refactored code and cleaned up spacing

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,68 +1,63 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
-//commenting for Austin
-//comment #2
-
-
 // create our typeDefs
 const typeDefs = gql`
-type User {
-  _id: ID
-  username: String
-  email: String
-  password: String
-  familyMembers: Int
-  products: [Product]
-  adoptedFamily: [User]
-}
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    familyMembers: Int
+    products: [Product]
+    adoptedFamily: [User]
+  }
 
-type Query {
-  users: [User]
-  user(username: String!): User
-  me: User
-  products(username: String): [Product]
-  product(productName: String!): Product
-  categories: [Category]
-  category(categoryName: String): Category
-  tags: [Tag]
-  tag(tagName: String!): Tag
-}
+  type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+    products(username: String): [Product]
+    product(productName: String!): Product
+    categories: [Category]
+    category(categoryName: String): Category
+    tags: [Tag]
+    tag(tagName: String!): Tag
+  }
 
-type Product {
-  _id: ID
-  productName: String
-  productNote: String
-  createdAt: String
-  users: [User]
-  tags: [Tag]
-}
+  type Product {
+    _id: ID
+    productName: String
+    productNote: String
+    createdAt: String
+    users: [User]
+    tags: [Tag]
+  }
 
-type Category {
-  _id: ID
-  categoryName: String
-  products: [Product]
-}
+  type Category {
+    _id: ID
+    categoryName: String
+    products: [Product]
+  }
 
-type Tag {
-  _id: ID
-  tagName: String
-}
+  type Tag {
+    _id: ID
+    tagName: String
+  }
 
-type Auth {
-  token: ID!
-  user: User
-}
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-type Mutation {
-        addUser(username: String!, email: String!, password: String!): User
-        login(email: String!, password: String!): Auth
-        addAdoptedFamily(adoptedFamilyId: ID!): User
-        deleteAdoptedFamily(adoptedFamilyId: ID!): User
-        addProduct(productName: String!, productNote: String!): Product
-        deleteProduct(productId: String!): Product
-    }
-
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): User
+    login(email: String!, password: String!): Auth
+    addAdoptedFamily(adoptedFamilyId: ID!): User
+    deleteAdoptedFamily(adoptedFamilyId: ID!): User
+    addProduct(productName: String!, productNote: String!): Product
+    deleteProduct(productId: String!): Product
+  }
 `;
 
 // export the typeDefs
