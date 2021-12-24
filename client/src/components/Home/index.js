e1fe115a1c112051f68ce9b157a8a65c93fe582b
jazzmbarry <jazzmbarry@gmail.com>
2021-12-23 19:41:00 -0700

    Update to deploy

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -7,9 +7,9 @@ export default function Home () {
             <title>
                 <h2>Home</h2>
             </title>
-            <div>
-                <img src={ HomeImage }></img>
-                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </h3>
+            <div className='columns'>
+                <img className='column is-half' src={ HomeImage }></img>
+                <h3 className='column'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </h3>
             </div>
         </div>
     )
