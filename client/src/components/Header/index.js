e19fb8dedad67563fa553bddd55a77523e4e41bc
jazzmbarry <jazzmbarry@gmail.com>
2021-12-28 10:42:59 -0700

    Update Login function

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Header (props) {
     return (
         <header className='login column'>
-            <a href='#login' onClick={() => props.handlePageChange('Login')}>Login</a>
+            <a 
+            href='#login' 
+            onClick={() => props.handlePageChange('Login')}
+            key={'Login'}>
+                Login
+                </a>
         </header>
     )
 }
