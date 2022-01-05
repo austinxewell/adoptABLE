4932933203c8b2a9c39bd24a7264cf8eed9e93b5
jazzmbarry <jazzmbarry@gmail.com>
2022-01-04 20:55:53 -0700

    working on front end Stripe

--- client/src/components/Donate/index.js
+++ client/src/components/Donate/index.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './donate.css'
+import { QUERY_CHECKOUT } from '../../utils/queries'
+import { loadStripe } from '@stripe/stripe-js'
+import { useLazyQuery } from '@apollo/client'
+const stripePromise = loadStripe('sk_test_4eC39HqLyjWDarjtT1zdp7dc')
 
-export default function Donate(){
+export default function Donate(props){
+  const [donateState, setDonateState] = useState({ donate : '' })
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
 
+  const submitCheckout = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await getCheckout({
+        variables: { ...donateState },
+      });
+      console.log([data])
+
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    // setDonateState({
+    //   donate : ''
+    // });
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setDonateState({
+      ...donateState,
+      [name]: value,
+    });
+  };
 
 
     return(
@@ -19,10 +59,10 @@ export default function Donate(){
                 <form>
                   <div>
                     <label className='columns is-centered donateLabel'>How much would you like to Donate?</label>
-                    <input className='donateInput' placeholder=''></input>
+                    <input className='donateInput' placeholder='' name='donate' type='donate' id='donate' value={donateState.donate} onChange={handleChange}></input>
                   </div>
                   <div className='columns is-centered'>
-                    <button className='donateSubmitButton' type="submit">Submit</button>
+                    <button className='donateSubmitButton' type="submit" onClick={submitCheckout}>Checkout</button>
                   </div>
                 </form>
               </div>
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -47,3 +47,11 @@ export const QUERY_ME_BASIC = gql`
         }
     }
 `;
+
+export const QUERY_CHECKOUT = gql`
+    query getCheckout($users: [ID]!) {
+        checkout(users: $users) {
+            session
+        }
+    }
+`
